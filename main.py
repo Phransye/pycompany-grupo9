e637ba54c6a9fd211dddb0d738ea108586210b5b
Francis Chavez Osses <francis.osses.t@gmail.com>
2025-10-28 16:40:16 -0300

    subir archivos
    resumen datos de empleados y mensaje final

---
+++ main.py
@@ -0,0 +1,6 @@
+
+def mostrar_mensaje_despedida():
+    print("\n👋 Gracias por trabajar con nosotros\n")
+    print("Ahi te ves")
+
+def menu 
---
+++ resumen.py
@@ -0,0 +1,14 @@
+
+
+def mostrar_resumen(nombre, edad, cargo, total):
+    print("\n📋 === RESUMEN DEL EMPLEADO ===\n")
+    print(f"Nombre: {nombre}")
+    print(f"Edad: {edad} años")
+    print(f"Cargo: {cargo}")
+    print(f"Sueldo total con bono: ${total}")
+
+
+
+
+
+
